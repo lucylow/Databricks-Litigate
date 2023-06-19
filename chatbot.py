46797174f28c0f9dd9afbe0c9169f42af16cfe2e
盧露 <low.lucyy@gmail.com>
2023-06-18 22:53:48 -0400

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from streamlit_chat import message
+
+with st.sidebar:
+    dolly_api_key = st.text_input('API Key',key='chatbot_api_key')
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title(" LITIGATE AI")
+#dolly.api_key = st.secrets.dolly_api_key
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+with st.form("chat_input", clear_on_submit=True):
+    a, b = st.columns([4, 1])
+    user_input = a.text_input(
+        label="Your message:",
+        placeholder="What would you like to say?",
+        label_visibility="collapsed",
+    )
+    b.form_submit_button("Send", use_container_width=True)
+
+for msg in st.session_state.messages:
+    message(msg["content"], is_user=msg["role"] == "user")
+
+if user_input and not dolly_api_key:
+    st.info("Please add your API key to continue.")
+    
+if user_input and dolly_api_key:
+    dolly.api_key = dolly_api_key
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    message(user_input, is_user=True)
+    response = dolly.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    message(msg.content)
